43f2fb96fbbde90fa2e82e619082cb6e661794aa
valerio <romavalerioio@gmail.com>
2022-12-08 21:37:38 +0000

    :sparkles: Handle game states

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
 const INITIAL_LENGTH = 4;
-const CELL_SIZE = 16;
-const CELLS = 40;
+const CELL_SIZE = 20;
+const CELLS = 30;
 const BOARD_SIZE = CELLS * CELL_SIZE;
 const initialSnake = Array.from({ length: INITIAL_LENGTH }, (_, i) => [
   0,
@@ -15,11 +15,21 @@ const overlap = (coords, [x2, y2]) =>
 const randomCoord = () => Math.floor(Math.random() * CELLS) * CELL_SIZE;
 
 export default function App() {
+  const [isGameInProgress, setIsGameInProgress] = useState(null);
   const [snake, setSnake] = useState(initialSnake);
   const [dirLen, setDirLen] = useState([1, CELL_SIZE]);
-  const [apple, setApple] = useState([randomCoord(), randomCoord()]);
-  const [score, setScore] = useState(0);
+  const [apple, setApple] = useState([null, null]);
   const [bump, setBump] = useState(-1);
+  const [score, setScore] = useState(0);
+
+  function startGame() {
+    setIsGameInProgress(true);
+    setSnake(initialSnake);
+    setDirLen([1, CELL_SIZE]);
+    setApple([randomCoord(), randomCoord()]);
+    setBump(-1);
+    setScore(0);
+  }
 
   function handleKeydown(e) {
     if (e.key === "ArrowRight" && dirLen[0] === 0) {
@@ -35,14 +45,15 @@ export default function App() {
 
   const snakeStyles = (top, left, i) => {
     return {
-      top: top + Math.random() * 2 + (i === bump - 1 ? -2 : 0),
-      left: left + Math.random() * 2 + (i === bump - 1 ? -2 : 0),
-      width: CELL_SIZE + (i === bump - 1 ? 0 : -4),
-      height: CELL_SIZE + (i === bump - 1 ? 0 : -4),
+      top: top + Math.random() * snake.length / 2 + (i === bump - 1 ? -4 : 0),
+      left: left + Math.random() * snake.length / 2 + (i === bump - 1 ? -4 : 0),
+      width: CELL_SIZE + (i === bump - 1 ? 0 : -8),
+      height: CELL_SIZE + (i === bump - 1 ? 0 : -8),
     };
   };
 
   const appleStyles = {
+    visibility: apple[0] === null ? "hidden" : "visible",
     top: apple[0],
     left: apple[1],
     width: CELL_SIZE - 4,
@@ -56,17 +67,20 @@ export default function App() {
     };
   }, [dirLen]);
 
-  //Handle movement
+  //Handle movement and death
   useEffect(() => {
-    const timerId = setTimeout(() => {
-      const head = [...snake.at(-1)];
-      const [dir, len] = dirLen;
-      head[dir] = (head[dir] + len + BOARD_SIZE) % BOARD_SIZE;
-      setSnake([...snake.slice(1), head]);
-      setBump(bump - 1);
-    }, 100 / (1 + score / 5));
-    return () => clearTimeout(timerId);
-  }, [snake, bump]);
+    if (isGameInProgress) {
+      setIsGameInProgress(!overlap(snake.slice(0, -1), snake.at(-1)));
+      const timerId = setTimeout(() => {
+        const head = [...snake.at(-1)];
+        const [dir, len] = dirLen;
+        head[dir] = (head[dir] + len + BOARD_SIZE) % BOARD_SIZE;
+        setSnake([...snake.slice(1), head]);
+        setBump(bump - 1);
+      }, 50 / (1 + score / 10));
+      return () => clearTimeout(timerId);
+    }
+  }, [isGameInProgress, snake, bump]);
 
   //Handle eating
   useEffect(() => {
@@ -77,7 +91,6 @@ export default function App() {
       setBump(snake.length);
     }
   }, [snake]);
-  // setIsGameOver(overlap(snake.slice(0, -1), head));
 
   //Handle growing
   useEffect(() => {
@@ -95,16 +108,30 @@ export default function App() {
 
   return (
     <>
-      <h1 className={`instructions${false ? "" : " hidden"}`}>
-        Use the arrow keys to start the game and move the snake
+      <h1
+        className={`instructions${isGameInProgress === null ? "" : " hidden"}`}
+      >
+        Click the button below to start the game. Use the arrow keys to move the
+        snake.
       </h1>
       <h3 className="score">score {score}</h3>
       <main className="playground">
         {snake.map(([top, left], i) => (
-          <div key={uuidv4()} className="snake" style={snakeStyles(top, left, i)} />
+          <div
+            key={uuidv4()}
+            className="snake"
+            style={snakeStyles(top, left, i)}
+          />
         ))}
         <div className="apple" style={appleStyles} />
       </main>
+      <button
+        className="start-button"
+        onClick={startGame}
+        disabled={isGameInProgress}
+      >
+        Start the game
+      </button>
     </>
   );
 }
